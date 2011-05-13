116e1266a1d6e11e4f4bbac7b1591d0c3370e2e2
Adam Shannon <adam@ashannon.us>
2011-05-12 22:33:52 -0500

    Start off index.php with a web access check and a i18n load.

---
+++ web/application/i18n/en.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Lystness
+ * Adam Shannon
+ */
+
+// Some global messages 
+define('PAGE_TITLE', 'Lystness -- The best todo list');
+define('WEB_ACCESS_DENIED', 'We\'re sorry, but web access to the site is currently disabled.');
