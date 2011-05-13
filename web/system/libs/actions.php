c5570d8e197f540d7e106d9c2878a9e56b70bb5e
Adam Shannon <adam@ashannon.us>
2011-05-13 14:48:09 -0500

    Implement two functions (genSalt & genHash) on sys:lib:Action.

--- web/system/libs/actions.php
+++ web/system/libs/actions.php
@@ -3,20 +3,141 @@
  * Lystness
  * Adam Shannon
  */
- 
-// genHash($str)
-// checkPassword($user_id, $submitted_pass);
-// getEmailWithId($user_id)
-// getIdWithEmail($email)
-// createUser($email, $pass, $timezone, $i18n)
-// updateUser($user_id, array([$user_id,][$email,][$password,][$salt,][$timezone,][$i18n,]))
-// authTagChange($user_id, $tag_id)
-// checkCookie($user_id, $cookie)
-// createCookie($user_id)
+
 class Action {
 
 	/**
 	 * genSalt($len = 50, $charset = 'a-zA-Z0-9')
+	 * Generate a salt, for hashing.
 	 */
+	static function genSalt($len = 50, $charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789') {
+		
+		$salt = '';
+		for ($i = 0; $i < $len; $i++) {
+			$salt .= substr($charset, mt_rand(0, strlen($charset)), 1);
+		}
+		return $salt;
+		
+	}
+	 
+	/**
+	 * genHash($pass)
+	 * Generate a sha1 hash from a given string.
+	 */
+	static function genHash($pass) {
+		return sha1($pass . self::genSalt());
+	}
+	
+	/**
+	 * getHashWithId($user_id)
+	 *
+	 */
+	static function getHashWithId($user_id) {
+		
+	}
+	
+	/**
+	 * checkPassword($user_id, $submitted_pass)
+	 *
+	 */
+	static function checkPassword($user_id, $submitted_pass) {
+		
+	}
+	
+	/**
+	 * getEmailWithId($user_id)
+	 * 
+	 */
+	static function getEmailWithId($user_id) {
+		
+	}
+	 
+	/**
+	 * getIdWithEmail($email)
+	 * 
+	 */
+	static function getIdWithEmail($email) {
+	
+	}
+	
+	/**
+	 * authTagChange($user_id, $tag_id)
+	 * 
+	 */
+	static function authTagChange($user_id, $tag_id) {
+		
+	}
+
+	/**
+	 * createCookie($user_id)
+	 *
+	 */
+	static function createCookie($user_id) {
+	
+	}
+	 
+	/**
+	 * checkCookie($user_id)
+	 *
+	 */
+	static function checkCookie($user_id) {
+		
+	}
+	 
+	/**
+	 * deleteCookie($user_id)
+	 *
+	 */
+	static function deleteCookie($user_id) {
+		
+	}
+	
+	/**
+	 * createUser($email, $submitted_pass, $timezone, $i18n)
+	 *
+	 */
+	static function createUser($email, $submitted_pass, $timezone, $i18n) {
+		
+	}
+	
+	/**
+	 * authUser($user_id)
+	 *
+	 */
+	static function authUser($user_id) {
+		
+	}
+	
+	/**
+	 * updateEmail($user_id, $new_email)
+	 *
+	 */
+	static function updateEmail($user_id, $new_email) {
+	
+	} 
+	
+	/**
+	 * updatePassword($user_id, $old_password, $new_password)
+	 *
+	 */
+	static function updatePassword($user_id, $old_password, $new_password) {
+		
+	}
+	
+	/**
+	 * updateTimezone($user_id, $timezone)
+	 *
+	 */
+	static function updateTimezone($user_id, $timezone) {
+	
+	}
+	
+	/**
+	 * updateI18n($user_id, $i18n)
+	 *
+	 */
+	static function updateI18n($user_id, $i18n) {
+		
+	}
 
 }
