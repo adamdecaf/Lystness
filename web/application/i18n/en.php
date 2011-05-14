e3995b58dcbfe09b8949670dd552ff42b5d679aa
Adam Shannon <adam@ashannon.us>
2011-05-13 22:29:57 -0500

    Create a useable front page for guests.

--- web/application/i18n/en.php
+++ web/application/i18n/en.php
@@ -7,3 +7,28 @@
 // Some global messages 
 define('SITE_TITLE', 'Lystness -- The best todo list');
 define('WEB_ACCESS_DENIED', 'We\'re sorry, but web access to the site is currently disabled.');
+
+// Nav Elements
+define('NAV_TOUR', 'Tour');
+define('NAV_CONTACT', 'Contact');
+define('NAV_LEGAL', 'Legal');
+
+// Footer
+define('FOOTER_COPYRIGHT', 'Copyright &copy; 2011 -- Adam Shannon -- All Rights Reserved');
+
+// home-guest.html
+define('HOME_GUEST_TITLE', 'Welcome to Lystness!');
+define('HOME_GUEST_DESC', 'Lystness is a todo application. It provides a service that is both easy to use and provides the needed features for you to get things done.');
+
+// Login
+define('LOGIN_EMAIL', 'Your Email: ');
+define('LOGIN_PASSWORD', 'Your Password: ');
+define('LOGIN_SUBMIT', 'Login');
+
+// Registering
+define('REGISTER_TITLE', 'Register');
+define('REGISTER_DESC', 'It looks like you haven\'t registered yet. It\'s <em>free</em> and takes <em>one</em> step, why not try it?');
+	define('REGISTER_EMAIL', 'Your Email: ');
+	define('REGISTER_PASSWORD', 'Your Password: ');
+	define('REGISTER_PASSWORD_VERIFY', 'Verify Password: ');
+	define('REGISTER_SUBMIT', 'Register');
